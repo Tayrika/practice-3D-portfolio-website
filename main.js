38284cef518318a328bb3947c689416a1bd9ce03
Tayrika <rika13tay@gmail.com>
2024-11-20 20:46:01 +0900

    Scroll Animatoin

--- main.js
+++ main.js
@@ -78,6 +78,25 @@ const moon = new THREE.Mesh(
 
 scene.add(moon);
 
+moon.position.z = 30;
+moon.position.setX(-10);
+
+// Scroll Animation
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  avatar.rotation.y += 0.01;
+  avatar.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
 // Game loop
 function animate() {
   requestAnimationFrame( animate );
